47d979f595b144df13ff0d1e3cb1a05a7cb74e0b
krifhiijj <140498604+krifhiijj@users.noreply.github.com>
2023-11-02 17:10:57 +0530

    uploading server.js file

---
+++ server.js
@@ -0,0 +1,29 @@
+//create a http server using node js
+
+const http=require('http');
+
+const{createFun,readFun,
+updateFun,deleteFun}=require('./newModule.js');
+
+const server=http.createServer(function(req,res){
+    console.log(req.method,req.url);
+
+    if(req.url == '/create'){
+      res.write(JSON.stringify(createFun(10)))
+    }else if(req.url == '/read'){
+      res.write(JSON.stringify(readFun()))
+    
+    }else if( req.url == '/update'){
+      res.write(JSON.stringify(updateFun(0,100)))
+    }else if(req.url == '/delete'){
+      res.write(JSON.stringify(deleteFun(0))) 
+    
+      
+    }
+      res.end();
+
+})
+
+server.listen(5000,function(){
+    console.log("server running on port 5000!")
+});
